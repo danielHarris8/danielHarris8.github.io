2e1f3c80135d9b6e3bbe15885745b3e3bc22e001
Danielgood12 <t105360709@ntut.org.tw>
2020-09-17 10:19:27 +0800

    delete

--- Js/blog.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function(){
-    if($(this).scrollTop()>500){
-      $('.navbar-b.navbar-trans').css('background-color','rgba(0,0,0,0.5)')
-    }else{
-      $('.navbar-b.navbar-trans').css('background-color','transparent')
-    }
-  })
